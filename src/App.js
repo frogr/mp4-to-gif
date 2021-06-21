5a6149fc518f998679d4916d404e0c916a9e3516
frogr <austindanielfrench@gmail.com>
2021-06-20 23:56:56 -0700

    load ffmpeg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
+const ffmpeg = createFFmpeg({ log: true })
+
 function App() {
+  const [ready, setReady] = useState(false);
+
+  const load = async () => {
+    await ffmpeg.load()
+    setReady(true)
+  }
+
+  useEffect(() => {
+    load();
+  }, [])
+
   return (
     <div className="App">
      
